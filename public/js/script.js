4fade5f95f38fd58c36ee772137dd1ea94263317
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2022-03-30 11:39:51 +0200

    forgot

---
+++ public/js/script.js
@@ -0,0 +1,172 @@
+function defaultHandler(response) {
+    if (response.status !== 200) {
+        if (response.message) {
+            if (typeof response.message === 'string') {
+                alert(response.message);
+            } else {
+                alert(JSON.stringify(response.message));
+            }
+        } else {
+            alert('Error: ' + response.status);
+        }
+    } else {
+        if (response.token) {
+            setToken(response.token);
+        }
+        let redirect = response.redirect;
+        if (!redirect) {
+            return;
+        }
+        window.location.href = redirect;
+    }
+}
+
+function post(url, data, callback) {
+    const xhr = new XMLHttpRequest();
+    if (!callback || typeof callback !== 'function') {
+        callback = defaultHandler;
+    }
+    //convert data to json if it is not already
+    if (typeof data === 'object') {
+        data = JSON.stringify(data);
+    }
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function () {
+        let res = JSON.parse(xhr.responseText);
+        res.status = xhr.status;
+        callback(res);
+    };
+    xhr.send(data);
+}
+
+function getToken() {
+    return sessionStorage.getItem('token') || localStorage.getItem('token');
+}
+
+function setToken(token) {
+    const field = document.getElementById('remember');
+    const remember = field ? field.checked : true;
+    if (remember) {
+        localStorage.setItem('token', token);
+    } else {
+        sessionStorage.setItem('token', token);
+    }
+}
+
+function loginByForm() {
+    post('/api/login', {
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+        remember: document.getElementById('remember').checked
+    });
+}
+
+function registerByForm() {
+    post('/api/register', {
+        name: document.getElementById('username').value,
+        email: document.getElementById('email_address').value,
+        phone: document.getElementById('phone').value,
+        password: document.getElementById('password').value,
+        password_confirmation: document.getElementById('password_confirmation').value,
+        remember: true
+    });
+}
+
+function get(url, callback) {
+    const xhr = new XMLHttpRequest();
+    if (!callback || typeof callback !== 'function') {
+        callback = defaultHandler;
+    }
+
+    xhr.open('GET', url, true);
+    const token = getToken();
+    if (token) {
+        xhr.setRequestHeader('Authorization', token);
+    }
+    xhr.onload = function () {
+        let res = JSON.parse(xhr.responseText);
+        res.status = xhr.status;
+        callback(res);
+    };
+    xhr.send();
+}
+
+function put(url, data, callback) {
+    const xhr = new XMLHttpRequest();
+    if (!callback || typeof callback !== 'function') {
+        callback = defaultHandler;
+    }
+
+    //convert data to json if it is not already
+    if (typeof data === 'object') {
+        data = JSON.stringify(data);
+    }
+    xhr.open('PUT', url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    const token = getToken();
+    if (token) {
+        xhr.setRequestHeader('Authorization', token);
+    }
+    xhr.onload = function () {
+        let res = JSON.parse(xhr.responseText);
+        res.status = xhr.status;
+        callback(res);
+    };
+    xhr.send(data);
+}
+
+function setDashboardContent() {
+    const username = document.getElementById("username");
+    const email = document.getElementById("email");
+    const phone = document.getElementById("phone");
+    const token = getToken();
+    if (token) {
+        put('/api/user', {
+            token: token,
+            username: username.value,
+            email: email.value,
+            phone: phone.value
+        }, function (response) {
+            if (response.status === 200) {
+                username.value = response.data.username;
+                email.value = response.data.email;
+                phone.value = response.data.phone;
+                alert("Successfully updated");
+            } else {
+                defaultHandler(response);
+            }
+        });
+    }
+}
+
+function refreshDashboardContent() {
+
+    get('/api/user',
+        function (response) {
+            if (response.status === 200) {
+                const username = document.getElementById("username");
+                const email = document.getElementById("email");
+                const phone = document.getElementById("phone");
+                username.value = response.data.username;
+                email.value = response.data.email;
+                phone.value = response.data.phone;
+                return
+            }
+            defaultHandler(response);
+        });
+
+}
+
+
+if (getToken()) {
+    document.getElementById("login").hidden = true;
+    document.getElementById("logout").hidden = false;
+    refreshDashboardContent()
+
+} else {
+    document.getElementById("login").hidden = false;
+    document.getElementById("logout").hidden = true;
+}
+
+
